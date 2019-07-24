2e460f891a5e7c0dc0be05d8eea79d7fb12d7495
Dextet0526 <soul9862@naver.com>
2019-07-24 15:30:19 +0900

    환경 설정(매핑)

--- Pr01/src/main/java/com/company/project/HomeController.java
+++ Pr01/src/main/java/com/company/project/HomeController.java
@@ -36,4 +36,10 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/index")
+	public String index(Model model) {
+		System.out.println("index");
+		return "index";
+	}
+	
 }
