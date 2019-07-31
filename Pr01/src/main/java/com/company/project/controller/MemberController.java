d9cabb2f2cfae9c96955a0b675419dcc96ca4647
Dextet0526 <soul9862@naver.com>
2019-07-31 15:20:16 +0900

    직원 화면 컨트롤러

---
+++ Pr01/src/main/java/com/company/project/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.company.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+
+	@RequestMapping(value = "/memberIndexView")
+	public String member_index_view(Model model) {
+		System.out.println("member index view");
+		
+		return "MemberView/memberIndexView";
+	}
+	
+	
+}
