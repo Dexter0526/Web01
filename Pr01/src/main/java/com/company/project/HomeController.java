738caac90f06e32561fc209fcedbebd7715b5ba5
Dextet0526 <soul9862@naver.com>
2019-08-01 14:55:39 +0900

    homecontroller 매핑 이전

--- Pr01/src/main/java/com/company/project/HomeController.java
+++ Pr01/src/main/java/com/company/project/HomeController.java
@@ -36,34 +36,5 @@ public class HomeController {
 		
 		return "home";
 	}
-
-	@RequestMapping(value = "/index")
-	public String index(Model model) {
-		System.out.println("index");
-		System.out.println("실행");
-		return "index";
-	}
-	
-	@RequestMapping(value = "/about")
-	public String about(Model model) {
-		System.out.println("about");
-		return "About/about";
-	}
-	@RequestMapping(value = "/contact")
-	public String contact(Model model) {
-		System.out.println("contact");
-		return "Contact/contact";
-	}
-	@RequestMapping(value = "/log")
-	public String log(Model model) {
-		System.out.println("log");
-		return "Log/log";
-	}
-	
-	@RequestMapping(value = "/join")
-	public String join(Model model) {
-		System.out.println("join");
-		return "Log/join";
-	}
 	
 }
