86910cb70d725fa948c21b3e72138cd887585178
Dextet0526 <soul9862@naver.com>
2019-08-12 14:40:18 +0900

    특허 db sql 추가

--- admin.sql
+++ admin.sql
@@ -1,15 +1,12 @@
-create user admin identified by 0000;
-grant connect, resource, dba to admin;
-
-
-create table member(
-    name varchar2(10),
-    email varchar2(30),
-    pwd varchar2(10),
-    phone char(13),
-    admin number(1) DEFAULT 1,
-    primary key(email)
+create table patent(
+    num number(5),
+    patentnum VARCHAR2(20) PRIMARY key,
+    title VARCHAR2(30),
+    content VARCHAR2(500),
+    readcount NUMBER(4) DEFAULT 0
 );
 
-INSERT INTO "ADMIN"."MEMBER" (NAME, EMAIL, PWD, PHONE, ADMIN) VALUES ('관리자', 'admin@dexterweb.co.kr', '0526', '010-1234-5678', '1');
+create SEQUENCE patent_seq start with 1 INCREMENT by 1;
 commit;
+
+insert into patent(num, patentnum, title, content) values(patent_seq.nextval, '158ci', '경량알루미늄', '경량 알루미늄 연결 강도 상승 및 비선형 구조물 강도 증가');
