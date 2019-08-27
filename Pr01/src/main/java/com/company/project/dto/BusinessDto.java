e07d66dd1b608842c8f3ad59b2f59d8e023887b6
Dextet0526 <soul9862@naver.com>
2019-08-27 16:56:58 +0900

    lombok 사용 @DATA

--- Pr01/src/main/java/com/company/project/dto/BusinessDto.java
+++ Pr01/src/main/java/com/company/project/dto/BusinessDto.java
@@ -1,52 +1,12 @@
 package com.company.project.dto;
 
+import lombok.Data;
+
+@Data
 public class BusinessDto {
 	private int num;
 	private String title;
 	private String content;
 	private String field;
-	
-	public BusinessDto() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public BusinessDto(int num, String title, String content, String field) {
-		super();
-		this.num = num;
-		this.title = title;
-		this.content = content;
-		this.field = field;
-	}
-
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getField() {
-		return field;
-	}
-	public void setField(String field) {
-		this.field = field;
-	}
-
-	@Override
-	public String toString() {
-		return "BusinessDto [num=" + num + ", title=" + title + ", content=" + content + ", field=" + field + "]";
-	}
-	
 	
 }
--- Pr01/src/main/java/com/company/project/dto/PatentDto.java
+++ Pr01/src/main/java/com/company/project/dto/PatentDto.java
@@ -1,44 +1,12 @@
 package com.company.project.dto;
 
+import lombok.Data;
+
+@Data
 public class PatentDto {
 	private int num;
 	private String patentNum;
 	private String title;
 	private String content;
 	
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public String getPatentNum() {
-		return patentNum;
-	}
-	public void setPatentNum(String patentNum) {
-		this.patentNum = patentNum;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public PatentDto(int num, String patentNum, String title, String content) {
-		super();
-		this.num = num;
-		this.patentNum = patentNum;
-		this.title = title;
-		this.content = content;
-	}
-	
-	public PatentDto() {
-		// TODO Auto-generated constructor stub
-	}
 }
