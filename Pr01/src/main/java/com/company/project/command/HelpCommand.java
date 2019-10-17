c7a29dc56c849599a35cfcfa3193b97987aacc4f
Dextet0526 <soul9862@naver.com>
2019-10-17 09:41:54 +0900

    버그 수정

--- Pr01/src/main/java/com/company/project/command/HelpCommand.java
+++ Pr01/src/main/java/com/company/project/command/HelpCommand.java
@@ -1,42 +1,26 @@
 package com.company.project.command;
 
-import org.apache.commons.mail.SimpleEmail;
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.company.project.dto.HelpDto;
 
 import lombok.extern.log4j.Log4j;
 
 @Log4j
+@Component
+@Transactional
 public class HelpCommand {
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
 	public void sendEmail(HelpDto helpDto, String action) {
-		log.info("이메일 전송 실행");
-		SimpleEmail email = new SimpleEmail();
-		email.setHostName("smtp.gmail.com");
-		email.setSmtpPort(465);
 		
-		email.setAuthentication("dexterpark1992@gmail.com", "jvvuyajvtgiuwvgw");
 		
-		email.setSSLOnConnect(true);
-		email.setStartTLSEnabled(true);
-		
-		String result = "fail";
-		
-		try {
-			email.setFrom("dexterpark1992@gmail.com", "덱스터 건설", "utf-8");
-			
-			// ex) 기술진 email
-			email.addTo("dexterpark1992@gmail.com", "덱스터 건설","utf-8");
-			
-			email.setSubject(helpDto.getTitle() + "  " + action);
-			email.setMsg("등록 번호 : " + helpDto.getNum() + 
-					"\n" + helpDto.getContent() + 
-					"\\n email : " + helpDto.getEmail());
-			
-			result = email.send();
-			log.info("메일 전송 결과 : " + result);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		log.info("이메일 전송 종료");
 	}
 }
